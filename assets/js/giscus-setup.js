5ab6b875bbb1dd18a68bafa47cacee382c35560b
marishkamehta <59931899+marishkamehta@users.noreply.github.com>
2026-05-23 04:48:54 +0000

    Deploying to gh-pages from @ marishkamehta/marishkamehta.github.io@bc6c83a05beb035c571ba0e650fa63df7a423c87 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "",
+    "data-repo": "marishkamehta/marishkamehta.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
